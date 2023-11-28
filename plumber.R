a66ad260d34053a2b31f1cfa409001d27acc18b6
Najmus Saqib <najsaqib@hotmail.com>
2023-11-28 12:14:06 -0500

    Develop endpoint for data extraction

--- plumber.R
+++ plumber.R
@@ -1,8 +1,16 @@
 # Import libraries
 library(plumber)
+library(tidyverse)
 
 # Import sample model
 model <- readRDS("trained_model.rds")
+
+# Import data the model was trained on and transform
+asbestos_raw <- read_csv("https://github.com/najsaqib/naj_lab/raw/main/asbestos.csv")
+
+asbestos_df <- asbestos_raw %>%
+  select(prcode, type, asbestos) %>%
+  mutate_if(is.character, as.factor)
 
 # Set up endpoints
 
@@ -19,4 +27,14 @@ status <- function(){
 #* @post /predict
 function(req, res){
   predict(model, new_data = req$body, type = "prob")
+}
+
+#* Extract filtered training data
+#* @param my_prcode
+#* @param my_type
+#* @get /data
+function(prcode, type) {
+  asbestos_df %>%
+    filter(.data$prcode == .env$prcode) %>%
+    filter(.data$type == .env$type)
 }
